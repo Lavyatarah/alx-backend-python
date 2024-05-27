7e72caf3ef8577f271880a0d81cd27b53ad8f4ac
Lavyatarah <mattlavyy@gmail.com>
2024-05-27 11:28:11 +0300

    updated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,7 +33,7 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), f"KeyError('{path[-1]}')")
+        self.assertEqual(str(cm.exception),  repr(path[-1]))
 
 
 class TestGetJson(unittest.TestCase):
